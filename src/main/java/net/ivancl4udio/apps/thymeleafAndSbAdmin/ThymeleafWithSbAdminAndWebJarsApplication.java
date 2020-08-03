c792d3da5fb5d922f8432e0aff475e85ebbfd09a
Ivan Cl4udio <ivan.claudio@gmail.com>
2020-08-03 10:50:36 +0000

    Removed unused imports from ThymeleafWithSbAdminAndWebJarsApplication

--- src/main/java/net/ivancl4udio/apps/thymeleafAndSbAdmin/ThymeleafWithSbAdminAndWebJarsApplication.java
+++ src/main/java/net/ivancl4udio/apps/thymeleafAndSbAdmin/ThymeleafWithSbAdminAndWebJarsApplication.java
@@ -1,9 +1,7 @@
 package net.ivancl4udio.apps.thymeleafAndSbAdmin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class ThymeleafWithSbAdminAndWebJarsApplication {
