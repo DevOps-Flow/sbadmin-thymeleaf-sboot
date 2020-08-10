5870f346ea0534ba6f759eb4c46fd397dd0b8c89
Ivan Claudio <ivan.claudio@gmail.com>
2020-08-10 08:03:36 -0300

    Removida internacionalização

--- src/main/java/net/ivancl4udio/apps/thymeleafAndSbAdmin/ServletInitializer.java
+++ src/main/java/net/ivancl4udio/apps/thymeleafAndSbAdmin/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ThymeleafWithSbAdminAndWebJarsApplication.class);
+		return application.sources(SBAdminJava.class);
 	}
 
 }
